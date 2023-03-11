d06189a25574e39ed15d7e585ff1008d5dbee30c
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-03-11 11:29:36 +0800

    feat: add find_user_by_phone

--- api/src/handler/auth.rs
+++ api/src/handler/auth.rs
@@ -2,6 +2,8 @@ use axum::{http::StatusCode, Json};
 use axum_sessions::extractors::{ReadableSession, WritableSession};
 use serde::Deserialize;
 
+use crate::service::user::find_user_by_phone;
+
 #[derive(Deserialize)]
 pub struct UserForm {
     phone_number: String,
@@ -12,7 +14,7 @@ pub async fn login(
     mut session: WritableSession,
     Json(payload): Json<UserForm>,
 ) -> (StatusCode, Json<serde_json::Value>) {
-    
+    find_user_by_phone(payload.phone_number).await;
     (
         StatusCode::NOT_FOUND,
         Json(serde_json::json!({ "status": "Not Found" })),
--- api/src/service/user.rs
+++ api/src/service/user.rs
@@ -1,9 +1,17 @@
 use anyhow::{Ok, Result};
 use entity::user;
 use entity::user::Entity as User;
+use migration::sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+use reels_config::contants::DB;
 
-pub async fn find_user_by_phone() -> Result<()> {
-    Ok(())
+pub async fn find_user_by_phone(phone: String) -> Result<Option<user::Model>> {
+    let db = DB.get().unwrap();
+    let user = User::find()
+        .filter(user::Column::PhoneNumber.eq(phone))
+        .one(db)
+        .await
+        .expect("Cannot retrieve user");
+    Ok(user)
 }
 
 pub async fn create_user() -> Result<()> {
