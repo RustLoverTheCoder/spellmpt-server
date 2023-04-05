d7a58c14045645d900a8ed2103746c84e52fdf7c
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-04-05 18:59:02 +0800

    feat: add user

--- api/src/handler/user.rs
+++ api/src/handler/user.rs
@@ -2,7 +2,14 @@ use axum::{http::StatusCode, response::IntoResponse, Json};
 use axum_sessions::extractors::ReadableSession;
 use uuid::Uuid;
 
-use crate::service::user::get_user_info_by_id;
+use crate::service::user::{get_user_info_by_id, update_user_info_by_id};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UpdateUserInfoRequest {
+    pub nickname: Option<String>,
+    pub phone_number: Option<String>,
+}
 
 pub async fn get_user_info(session: ReadableSession) -> impl IntoResponse {
     if let Some(user_id) = session.get::<Uuid>("id") {
@@ -16,9 +23,23 @@ pub async fn get_user_info(session: ReadableSession) -> impl IntoResponse {
     }
 }
 
-pub async fn update_user_info() -> impl IntoResponse {
-    (
-        StatusCode::NOT_FOUND,
-        Json(serde_json::json!({ "status": "Not Found" })),
-    )
+pub async fn update_user_info(
+    session: ReadableSession,
+    req: Json<UpdateUserInfoRequest>,
+) -> (StatusCode, Json<serde_json::Value>) {
+    if let Some(user_id) = session.get::<Uuid>("id") {
+        let user_info = get_user_info_by_id(user_id).await.unwrap();
+        match user_info {
+            None => return (StatusCode::NOT_FOUND, Json(serde_json::json!({}))),
+            Some(_) => {
+                let user =
+                    update_user_info_by_id(user_id, req.nickname.clone(), req.phone_number.clone())
+                        .await
+                        .unwrap();
+                (StatusCode::OK, Json(serde_json::json!({ "user": user })))
+            }
+        }
+    } else {
+        (StatusCode::UNAUTHORIZED, Json(serde_json::json!({})))
+    }
 }
--- api/src/service/user.rs
+++ api/src/service/user.rs
@@ -1,12 +1,12 @@
 use anyhow::{Ok, Result};
 use chrono::Utc;
+use config::contants::DB;
 use entity::user;
 use entity::user::Entity as User;
 use migration::{
     sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter, Set},
     DbErr,
 };
-use config::contants::DB;
 use uuid::Uuid;
 
 pub async fn find_user_by_phone(phone: String) -> Result<Option<user::Model>> {
@@ -40,6 +40,22 @@ pub async fn get_user_info_by_id(user_id: Uuid) -> Result<Option<user::Model>, D
     user
 }
 
-pub async fn update_user_info() -> Result<()> {
-    Ok(())
+pub async fn update_user_info_by_id(
+    user_id: Uuid,
+    nickname: Option<String>,
+    phone_number: Option<String>,
+) -> Result<user::Model, DbErr> {
+    let db = DB.get().unwrap();
+    let mut update_user = user::ActiveModel {
+        id: Set(user_id.to_owned()),
+        ..Default::default() // all other attributes are `NotSet`
+    };
+    if let Some(nickname) = nickname {
+        update_user.nickname = Set(nickname.to_owned());
+    }
+    if let Some(phone_number) = phone_number {
+        update_user.phone_number = Set(phone_number.to_owned());
+    }
+    let result = update_user.update(db).await;
+    return result;
 }
