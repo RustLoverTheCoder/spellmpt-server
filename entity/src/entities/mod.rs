8b3552de31fa3f3446c48b8cb5d392d77e20e0b0
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-04-05 22:25:37 +0800

    feat: add tag

--- entity/src/entities/mod.rs
+++ entity/src/entities/mod.rs
@@ -3,4 +3,5 @@
 pub mod prelude;
 
 pub mod prompt;
+pub mod tag;
 pub mod user;
--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20230310_032859_create_user_table;
 mod m20230405_111835_create_prompt_table;
+mod m20230405_142124_create_tag_table;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20230310_032859_create_user_table::Migration),
             Box::new(m20230405_111835_create_prompt_table::Migration),
+            Box::new(m20230405_142124_create_tag_table::Migration),
         ]
     }
 }
